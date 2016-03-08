3e63c78402854c4174f288dc5cbc94851023fb27
the-platcal <aaron.b99@gmail.com>
2016-03-07 22:49:37 -0800

    gimbal and wind vane imple

---
+++ lib/wind-vane/wind-vane.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
+# This code is released into the public domain
+
+import time
+import os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+DEBUG = 1
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# wind vane signale connected to adc #0
+wind_vane_adc_pin = 0;
+
+last_read = 0       # this keeps track of the last potentiometer value
+tolerance = 3       # to keep from being jittery we'll only change
+                    # volume when the pot has moved more than 5 'counts'
+
+while True:
+        # we'll assume that the pot didn't move
+        wind_vane_changed = False
+
+        # read the analog pin
+        wind_direction = round(readadc(wind_vane_adc_pin, SPICLK, SPIMOSI, SPIMISO, SPICS) * 0.17578125)
+        # how much has it changed since the last read?
+        wind_change = abs(wind_direction - last_read)
+
+        # if DEBUG:
+        #         print "wind_direction:", wind_direction
+
+        if ( wind_change > tolerance ):
+               wind_vane_changed = True
+
+        # if DEBUG:
+        #         print "wind_vane_changed", wind_vane_changed
+
+        if ( wind_vane_changed ):
+                if DEBUG:
+                        # print "set_volume", set_volume
+                        print "write file here: ", wind_direction
+
+                # save the potentiometer reading for the next loop
+                last_read = wind_direction
+
+        # hang out and do nothing for a half second
+        time.sleep(1)
--- weather-station.js
+++ weather-station.js
@@ -1,60 +1,166 @@
 /**
- * Created by aaron on 11/28/15.
+ * Created by aaron on 1/17/16.
  */
-var express = require('express')
-    , routes = require('./routes')
-    , http = require('http')
-    , path = require('path')
-    , gpio = require("rpi-gpio")
-    , ds18b20 = require('ds18b20')
-    , logger = require('./lib/logger');
-
-
+var express = require('express');
+var routes = require('./routes');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var path = require('path');
+var pkg = require('./package');
 
-app.configure(function () {
-    app.set('port', 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.locals.ds18b20 = ds18b20;
-    app.locals.logger = logger;
-    app.locals.weather = {temp: NaN};
-    app.locals.pollingInterval = 1000;
+app.use('/', express.static(pkg.ramDiskPath));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 
-    app.locals.logger.init(app);
-    app.locals.logger.start();
-});
-
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
-
+//app.get('/', function(req, res) {
+//    res.sendFile(__dirname + '/index.html');
+//    //console.log(path.join(__dirname, '/stream'));
+//});
 
 app.get('/', function (req, res) {
-    res.render('index', {title: require('./package.json').name, port: app.get('port')});
+    res.render('index', {title: pkg.name, version: pkg.version});
 });
 
-app.get('/logging/start', function (req, res) {
-    app.locals.logger.start();
-    res.send('started');
+
+var sockets = {};
+
+io.on('connection', function (socket) {
+    sockets[socket.id] = socket;
+    //console.log("Total clients connected : ", Object.keys(sockets).length);
+
+    socket.on('disconnect', function () {
+        delete sockets[socket.id];
+        //console.log('socket.on disconnect');
+        //console.log("Total clients connected : ", Object.keys(sockets).length);
+
+        stopStreaming();
+
+        // no more sockets, kill the stream
+        if (Object.keys(sockets).length == 0) {
+            app.set('isStreaming', false);
+            //if (proc) proc.kill();
+            fs.unwatchFile(pkg.ramDiskPath + '/image_stream.jpg');
+        }
+    });
+
+    socket.on('start-stream', function () {
+        startStreaming(io);
+    });
+
 });
 
-app.get('/logging/stop', function (req, res) {
-    app.locals.logger.stop();
-    res.send('stopped');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
-app.get('/current-temp', function (req, res) {
-    var log = app.locals.logger.log();
-    console.log(log[log.length-1].temp)
-    res.send(String(log[log.length-1].temp) + 'c : ' + String(log[log.length-1].temp * 1.8 + 32) + 'f');
+function stopStreaming() {
+    if (Object.keys(sockets).length == 0) {
+        app.set('isStreaming', false);
+        //if (proc) proc.kill();
+        fs.unwatchFile(pkg.ramDiskPath + '/image_stream.jpg');
+    }
+}
 
-});
+function startStreaming(io) {
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
-});
+    if (app.get('isStreaming')) {
+        io.sockets.emit('liveStream', './image_stream.jpg?_t=' + (Math.random() * 100000));
+        return;
+    }
 
+    app.set('isStreaming', true);
+
+    fs.watchFile(pkg.ramDiskPath + '/image_stream.jpg', function (current, previous) {
+        //console.log('watch hit:' + current + " : " + previous);
+        io.sockets.emit('liveStream', './image_stream.jpg?_t=' + (Math.random() * 100000));
+    });
+}
+
+
+///**
+// * Created by aaron on 11/28/15.
+// */
+//var express = require('express')
+//    , routes = require('./routes')
+//    , http = require('http')
+//    , path = require('path')
+//    , gpio = require("rpi-gpio")
+//    , ds18b20 = require('ds18b20')
+//    , logger = require('./lib/logger');
+//
+//
+//var app = express();
+//
+//app.configure(function () {
+//    app.set('port', 3000);
+//    app.set('views', __dirname + '/views');
+//    app.set('view engine', 'jade');
+//    app.use(express.favicon());
+//    app.use(express.logger('dev'));
+//    app.locals.ds18b20 = ds18b20;
+//    app.locals.logger = logger;
+//    app.locals.weather = {temp: NaN};
+//    app.locals.pollingInterval = 5000;
+//
+//
+//    app.locals.logger.init(app);
+//    app.locals.logger.start();
+//});
+//
+//app.configure('development', function () {
+//    app.use(express.errorHandler());
+//});
+//
+//
+//app.get('/', function (req, res) {
+//    res.render('index', {title: require('./pkg.json').name, port: app.get('port')});
+//});
+//
+//app.get('/logging/start', function (req, res) {
+//    app.locals.logger.start();
+//    res.send('started');
+//});
+//
+//app.get('/logging/stop', function (req, res) {
+//    app.locals.logger.stop();
+//    res.send('stopped');
+//});
+//
+//
+//app.get('/current-temp', function (req, res) {
+//    var log = app.locals.logger.log();
+//    var current = log[log.length - 1].temp;
+//    var high = 0;
+//    var low = 0;
+//
+//    if (log.length == 1) {
+//        high = log[0].temp;
+//        low = log[0].temp;
+//    } else {
+//        for (var i = 0; i < log.length; i++) {
+//            if (log[i].temp >= high) {
+//                high = log[i].temp;
+//            }
+//
+//            if (log[i].temp <= low) {
+//                low = log[i].temp;
+//            }
+//        }
+//    }
+//
+//
+//    res.send(String(current) + 'c : ' + String(current * 1.8 + 32) + 'f<br>' +
+//        'high: ' + String(high) + '<br>' +
+//        'low: ' + String(low));
+//
+//
+//});
+//
+//http.createServer(app).listen(app.get('port'), function () {
+//    console.log("Express server listening on port " + app.get('port'));
+//});
+//
